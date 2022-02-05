25ef2e15adcdf9abfe7208b1fd7111958e5477f6
san199 <35841544+san199@users.noreply.github.com>
2022-02-05 21:55:12 +0545

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  env: {
+      MONGO_URI: "mongodb+srv://mongotest:mongotest@cluster0.lxwtw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  }
+}
---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import 'semantic-ui-css/semantic.min.css'
+import '../styles/style.css';
+import Layout from '../components/Layout';
+
+function MyApp({ Component, pageProps }) {
+  return <Layout><Component {...pageProps} /></Layout>
+}
+
+export default MyApp
